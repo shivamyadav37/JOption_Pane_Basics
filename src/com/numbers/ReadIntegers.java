f48852875f38e292883cd9bb1b5b1757a82ea1c0
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-04-02 10:05:42 +0530

    Operating On Integers

--- src/com/numbers/ReadIntegers.java
+++ src/com/numbers/ReadIntegers.java
@@ -2,6 +2,11 @@ package com.numbers;
 
 public class ReadIntegers {
     public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+
+        String input;
+
 
     }
 
