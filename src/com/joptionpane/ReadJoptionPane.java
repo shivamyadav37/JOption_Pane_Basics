fd824ed7ef2617f636cf9ce831e456d614e210cb
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-04-02 09:41:49 +0530

    intialising Variables

--- src/com/joptionpane/ReadJoptionPane.java
+++ src/com/joptionpane/ReadJoptionPane.java
@@ -1,9 +1,14 @@
 package com.joptionpane;
 
+import javax.swing.*;
+
 public class ReadJoptionPane {
     public static void main(String[] args) {
         String firstName;
         String lastName;
+
+        firstName = JOptionPane.showInputDialog("Please Enter the First Name ");
+
 
 
     }
